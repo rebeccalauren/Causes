bc54cdcaee0107fccf2fcce043a33119c1faf1ae
CodeoRodeo <hommenerik@gmail.com>
2016-03-09 01:08:27 -0500

    Added files via upload

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
 
-var imagecount = 1;
-var total = 5;
-
-function slide(x) {
-    var Image = document.getElementById('img');
-    imagecount = imagecount + x;
-      if (imagecount > total) { imagecount = 1;}
-      if (imagecount < 1) { imagecount = total;}
-    Image.src ="images/img"+ imagecount + ".jpg";
-    }
-window.setinterval(function slideA() {
-    var Image = document.getElementById('img');
-    imagecount = imagecount + 1;
-      if (imagecount > total) { imagecount = 1;}
-      if (imagecount < 1) { imagecount = total;}
-    Image.src ="images/img"+ imagecount + ".jpg";
-
-    },2000);
 
 $(function(){
   $('.fadein img:gt(0)').hide();
@@ -50,4 +32,3 @@ var q=25000;
     } else {
         image.src = "images/thermometer.gif";
  }}
-
