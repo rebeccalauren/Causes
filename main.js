2ff25be0de8092e056fe27afa3ccf2bc47c2d499
rebeccalauren <rebeccalauren@gmail.com>
2016-03-10 19:33:07 -0500

    Added files via upload

--- main.js
+++ main.js
@@ -1,38 +1,23 @@
 $(document).ready(function() {
 
-$("#myDivred1").animate({
-        width: "70%",
-      } 
-  , 2000);
+    $("#myDivred1").animate({
+          width: "70%",
+        } 
+    , 2000);
+
+    $("#countdown").countdown({until: new Date(2016, 5, 1), 
+      format:"dhMS"});
+
+    $(function(){
+      $('.fadein img:gt(0)').hide();
+
+      setInterval(function(){
+        $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');
+    }, 3000);
+    });
 });
-
-/*$("#clock").countdown({
-  until: new Date(2016, 5, 1), format:"dhM"});*/
-
-setInterval(
-  function(){
-    var dateTime = new Date();
-    var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
-    $("#milliseconds").text(dateTime.getMilliseconds());
-    $("#seconds").text(dateTime.getSeconds());
-    $("#minutes").text(dateTime.minutesTwoDigitsWithLeadingZero);
-    $("#hours").text(dateTime.getHours());
-  },
-  500)
-
-//$(selector).countdown({until: liftoffTime, format: 'dHM'});
-//$(selector).countdown({since: new Date(2014, 12-1, 25)});
-//dec 25 2014
-
-$(function(){
-  $('.fadein img:gt(0)').hide();
-  setInterval(function(){
-  $('.fadein :first-child').fadeOut()
-  .next('img').fadeIn()
-  .end().appendTo('.fadein');},
-  3000);
-});
-
 
 
 
